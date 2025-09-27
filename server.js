2e92715d2a803a261855bbaa800beae4939f6118
khushalkothiya5 <khushalkothiya5@gmail.com>
2025-09-28 04:17:00 +0530

    ok

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   tls: true,
 });
 
